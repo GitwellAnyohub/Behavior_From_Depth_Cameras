cacfde1cf0a73575635c647af1b7f3a8bcc0256d
Rockwell <rockwell.anyoha@gmail.com>
2018-04-11 22:00:29 -0400

    in order to test my rs-capture scripts, I am creating a new folder. I'm basically finished with the pipeline, I just need to change the way I save the files and make it as fast as possible

---
+++ experiment_depth_1/rs-capure-8.cpp
@@ -0,0 +1,101 @@
+																																																																																																																					//////saving to disk
+
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+void save_frame_depth_data(const std::string& filename, // save depth
+                           rs2::frame frame,
+                           float depth_units)
+{
+    if (auto image = frame.as<rs2::video_frame>())
+    {
+        std::ofstream myfile;
+        std::stringstream fullname;
+        fullname << filename << "_" << frame.get_frame_number() << ".csv";
+        myfile.open(fullname.str());
+        myfile << std::setprecision(2);
+        auto pixels = (uint16_t*)image.get_data();
+        
+        for (auto y = 0; y < image.get_height(); y++)
+        {
+            for (auto x = 0; x < image.get_width(); x++)
+            {
+                myfile << pixels[image.get_width() * y + x] * depth_units << ", ";
+            }
+            myfile << "\n";
+        }
+        
+        myfile.close();
+    }
+}
+
+void save_frame_color_data(const std::string& filename,//save color
+                           rs2::frame frame)
+{
+    if (auto image = frame.as<rs2::video_frame>())
+    {
+        std::ofstream myfile;
+        std::stringstream fullname;
+        fullname << filename << "_" << frame.get_frame_number() << ".csv"; //Append File Name
+        myfile.open(fullname.str());
+        myfile << std::hex << std::setw(2) << std::setfill('0');
+        auto pixels = (uint8_t*)image.get_data();
+        
+        for (auto y = 0; y < image.get_height(); y++)
+        {
+            for (auto x = 0; x < image.get_width(); x++)
+            {
+                myfile << "0x";
+                for (auto c = 0; c < 3; c++)
+                {
+                    myfile << (int)pixels[3*(image.get_width() * y + x) + c];
+                }
+                myfile << ",";
+            }
+            myfile << "\n";
+        }
+        
+        myfile.close();
+    }
+}
+
+// Capture Example demonstrates how to
+// capture depth and color video streams and render them to the screen
+int main(int argc, char * argv[]) try
+{
+    rs2::colorizer color_map;
+    
+    auto depth_units = rs2::context().query_devices().front()
+        .query_sensors().front().get_option(RS2_OPTION_DEPTH_UNITS);
+    
+    rs2::pipeline pipe;
+    rs2::config myconfig; //I am declaring the configuration object
+    myconfig.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 90); //This is the configuration that I want. I adding this stream to my configuration. I chose UYVY format because I heard frames might drop on linux otherwise
+    pipe.start(myconfig);
+
+    while(true) // Application still alive?
+    {
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+	rs2::frame depth = data.get_depth_frame(); // Find and colorize the depth data
+
+      
+        save_frame_depth_data("depth", depth, depth_units);
+      
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
