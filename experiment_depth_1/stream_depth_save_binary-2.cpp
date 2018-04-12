7b275bc3717d2fe1b07cefa7af3ffc3741e56a63
Rockwell <ranyoha@g.harvard.edu>
2018-04-12 15:55:26 -0400

    this is da real deal baby

--- experiment_depth_1/stream_depth_save_binary-2.cpp
+++ experiment_depth_1/stream_depth_save_binary-2.cpp
@@ -14,9 +14,8 @@ bool save_frame_raw_data(const std::string& filename, rs2::frame frame)
     auto image = frame.as<rs2::video_frame>();
     if (image)
     {
-        std::ofstream outfile(filename.data(), std::ofstream::binary);
-        outfile.write(static_cast<const char*>(image.get_data()), image.get_height()*image.get_stride_in_bytes());
-	outfile.close();
+        std::ofstream outfile(filename.data(), std::ofstream::binary | std::ofstream::app); //the std::ofstream::app is key as it allows me to append!
+        outfile.write(static_cast<const char*>(image.get_data()), image.get_height()*image.get_stride_in_bytes()); //as you can see I do not close the file. This is not good practice. I need to add a signal function that closes the file when I hit ctrl-C AND then terminates the program (maybe even with a message)	
         ret = true;
     }
 
@@ -34,7 +33,9 @@ int main(int argc, char * argv[]) try
         .query_sensors().front().get_option(RS2_OPTION_DEPTH_UNITS);
     
     rs2::pipeline pipe;
-    pipe.start();
+    rs2::config myconfig; //I am declaring the configuration object
+    myconfig.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 90); //This is the configuration that I want. I adding this stream to my configuration. I chose UYVY format because I heard frames might drop on linux otherwise
+    pipe.start(myconfig);
 
     while(true) // Application still alive?
     {
