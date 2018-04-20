36b82fec7013ebb57892ddc3d6d7e05226c11f33
wingillis <win.gillis@gmail.com>
2018-04-20 11:12:25 -0400

    ingnore xcode builds

---
+++ win/mousesense/mousesense/fileSaver.cpp
@@ -0,0 +1,9 @@
+//
+//  fileSaver.cpp
+//  mousesense
+//
+//  Created by Winthrop Gillis on 4/20/18.
+//  Copyright © 2018 Winthrop Gillis. All rights reserved.
+//
+
+#include "fileSaver.hpp"
---
+++ win/mousesense/mousesense/fileSaver.hpp
@@ -0,0 +1,14 @@
+//
+//  fileSaver.hpp
+//  mousesense
+//
+//  Created by Winthrop Gillis on 4/20/18.
+//  Copyright © 2018 Winthrop Gillis. All rights reserved.
+//
+
+#ifndef fileSaver_hpp
+#define fileSaver_hpp
+
+#include <stdio.h>
+
+#endif /* fileSaver_hpp */
